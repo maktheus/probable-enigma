03a7decb7d9a32e5a7db8776193b4fd2f60497e6
Uchoa <maktheus@gmail.com>
2021-12-02 14:53:55 -0400

    update

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,33 +1,23 @@
 import { Router } from "express";
+import { Category } from "../model/Category";
 
 const router = Router();
 
-const categories = [];
-
-
-
-
-
+const categories: Category[] = [];
 
 router.post("/categories", (req, res) => {
+    const { name, description } = req.body;
 
-    const { name,description } = req.body;
+    const category = new Category();
 
-    categories.push({
-        name,
-        description
-    });
+    Object.assign(category, { name, description,created_at: new Date()});
+
+    categories.push(category);
 
     return res.status(201).send({
         message: "Categoria cadastrada com sucesso",
-        data: categories
+        data: categories,
     });
-    
 });
 
-
-
-
-
-
-export {router as categoriesRouter};
+export { router as categoriesRouter };
