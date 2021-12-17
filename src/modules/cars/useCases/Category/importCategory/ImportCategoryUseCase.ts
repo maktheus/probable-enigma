5348b47486bc772dcd19e285919a000dbec528e7
Uchoa <maktheus@gmail.com>
2021-12-17 18:10:26 -0400

    update

--- src/modules/cars/useCases/Category/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/Category/importCategory/ImportCategoryUseCase.ts
@@ -1,16 +1,44 @@
-import { parse } from 'csv-parse';
+import { parse } from "csv-parse";
 import fs from "fs";
-class ImportCategoryUseCase{
-    execute(file:Express.Multer.File):void{
-        const stream = fs.createReadStream(file.path);
-        const parsefile = parse();
-        stream.pipe(parsefile);
+import { ICategoryRepository } from "../../../repositories/implemetantions/ICategoriesRepository";
 
-        parsefile.on("data",async (line)=>{
-            console.log(line);
-        })
+interface IImportCategory {
+    name: String;
+    description: string;
+}
+class ImportCategoryUseCase {
+    constructor(private iCategoriesRepository: ICategoryRepository) {}
+
+    loadCategories(file: Express.Multer.File):Promisse<IImportCategory[]> {
+        return new Promise((res, rej) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
+
+            const parsefile = parse();
+            stream.pipe(parsefile);
+
+            parsefile.on("data", async (line) => {
+                const [name, description] = line;
+                categories.push({
+                    name,
+                    description,
+                });
+            })
+            .on("end",()=>{
+                res(categories)
+            })
+            .on("error",(err)=>{
+                rej(err);
+            })
+        });
+
+       
     }
 
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+        console.log(categories);
+    }
 }
 
-export {ImportCategoryUseCase}
+export { ImportCategoryUseCase };
--- src/modules/cars/useCases/Category/importCategory/index.ts
+++ src/modules/cars/useCases/Category/importCategory/index.ts
@@ -1,8 +1,10 @@
+import { CategoriesRepositories } from "../../../repositories/CategoriesRepositories";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
 
-const importCategoryUseCase = new ImportCategoryUseCase();
+const categoriesRepository = CategoriesRepositories.getInstance();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
 export { importCategoryController };
